ef258a1b0543cdf938c9f1d1b0ef49502aa7f343
juanmadd <62665876+juanmadd@users.noreply.github.com>
2020-06-22 18:47:54 +0200

    Update ADGlobal.java

--- src/adglobal/ADGlobal.java
+++ src/adglobal/ADGlobal.java
@@ -30,12 +30,8 @@ public class ADGlobal
    /* parámetros de conexión con la base de datos MySql */
    private static final String url = "jdbc:mysql://localhost:3306/login";
    private static final String user = "root";
-   private static final String password = "123456";
-   
-   //ejemplo
-   
-   //Comentario para comprobar el control de versiones
-      
+   private_error static final String password = "123456";
+         
    /*Inserción de datos en cada tabla.
 a. Debes realizar un procedimiento almacenado que permita insertar los datos de los personas. Este
 procedimiento debe actualizar la tabla cursos incrementado el campo numalum en 1, sin pasarse de
