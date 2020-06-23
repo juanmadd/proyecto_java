0aec2e91bf5eea1affda7c50800a2b5eeff06c5c
juanmadd <62665876+juanmadd@users.noreply.github.com>
2020-06-23 10:31:34 +0200

    Update ADGlobal.java

--- src/adglobal/ADGlobal.java
+++ src/adglobal/ADGlobal.java
@@ -30,7 +30,7 @@ public class ADGlobal
    /* parámetros de conexión con la base de datos MySql */
    private static final String url = "jdbc:mysql://localhost:3306/login";
    private static final String user = "root";
-   private static final String password = "123456";
+   private_error static final String password = "123456";
    
    //Ejemplo
          
